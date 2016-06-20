2185f7ffc4e50b7722bdee82dfcba9096cd7c886
nisimakihiroki1205 <g1527055@st.t-kougei.ac.jp>
2016-06-20 17:27:07 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,5 +1,5 @@
 var i = Math.floor(0);
-//for(i=0;i<3;i++){
+for(i=0;i>0;i++){
 var result = window.confirm('じゅんびOK');
 
 var number = Math.floor(Math.random()*3);
@@ -22,4 +22,4 @@ else{
 
 
 document.getElementById('choice').textContent = message;
-//}
+}
